3a633bb94bf7c7117b45638532615ea66117fc1e
Roman Blanco <roman.blanco92@gmail.com>
2014-06-30 16:46:29 +0200

    Prepsani skriptu

--- README.md
+++ README.md
@@ -3,11 +3,11 @@ VUT FIT WIS Checker
 
 Funkce a možnosti
 -----------------
-
 - skript stahuje aktualní hodnocení z WISu a porovnává, zda se hodnocení oproti původnímu načtení změnilo
 - lze nastavit libovolný příkaz na upozornění změny hodnocení
 - možnost nastavit zobrazení hodnocení konkrétních termínů
-- možnost nastavení spouštění skriptu ve smyčce s libovolnou frekvencí
+- možnost nastavení frekvence aktualizace
+
 
 Konfigurace
 -----------
@@ -21,50 +21,93 @@ Pro nastavení skriptu se používá soubor [wis.ini](/wis.ini "wis.ini")
     + `pass` — heslo k informačnímu systému
 
  * Funkčnost a způsob spouštění skriptu:
-    + `check` — zapnutí kontrolování nového hodnocení
-    + `loop` — zapnutí spouštění ve smyčce (pokud neni zapnuté, `loopTime` nemá vliv )
     + `time` — pauza mezi opětovným spuštěním při spouštění ve smyčce (v sekundách)
-    + `cmd` — přikaz, který se má použít jako notifikace
+    + `cmd` — přikaz operačního systému, který se má použít jako notifikace
 
 ##### Sekce `[courses]` a `[terms]`: #####
 
  * Nastavení vypisovaných dat:
-    + `course[n]` — zkratky vypisovaných předmětů (zkratky je potřeba zadat přesně jako je ve WISu)
-    + `terms[n]` — URL adresy sledovaných termínů
+    + `course` — zkratky vypisovaných předmětů (oddělené novým řádkem, viz Ukázka konfigurace)
+    + `terms` — URL adresy sledovaných termínů (taktéž oddělené novým řádkem)
     
 #### Ukázka konfigurace: ####
     [WIS]
     user = xlogin00
-    pass = he5lonaw1s
-    check = True
-    loop = True
+    pass = he5lonawis
     time = 90
     cmd = aplay band.wav
 
     [courses]
-    course[0] = INP
-    course[1] = INM
-    course[2] = IFJ
-    course[3] = IAL
-    course[4] = ISS
-    course[5] = IKPT
+    course = INP
+             INM
+             IFJ
+             IAL
+             ISS
 
     [terms]
-    term[0] = https://wis.fit.vutbr.cz/FIT/st/course-sl.php?id=525641&item=45878
-    term[1] = https://wis.fit.vutbr.cz/FIT/st/course-sl.php?id=525641&item=46178
+    term =   https://wis.fit.vutbr.cz/FIT/st/course-sl.php?id=543210&item=01234
+             https://wis.fit.vutbr.cz/FIT/st/course-sl.php?id=012345&item=43210
 
-Pokud prvky v konfiguraci chybí, nebo konfigurační soubor chybí úplně, skript použije defaultní nastavení:
+Pokud některé prvky v konfiguraci chybí, nebo konfigurační soubor chybí úplně, skript se chová následovně:
 
-    check = True
-    loop = True
-    time = 300
-    
-   Přihlašovací údaje a vypisované kurzy uživatel bude moct zadat po výzvě
+- pro přihlašovací údaje bude skript vyžadovat ruční zadání (viz Ukázka spouštění)
+- pro ostatní skript použije defaultní nastavení:
 
-#### Upozornění: ####
-Skript kontroluje nové hodnocení v rámci celého předmětu. Pokud nebude předmět, jehož termín je zobrazovaný, mezi zobrazovanými předměty (sekce `[courses]`), nebude na změnu hodnocení upozorněno
+        time = 300
+        cmd = None
+        courses = [všechny předměty v aktualním ak. roce]
+        term = None
 
-Screenshot
-----------
 
-![WIS Checker running example](scrsht.png)  
+Spouštění
+---------
+
+Při spuštění skriptu bez parametru, začne skript ve smyčce kontrolovat nové hodnocení, v případě změny hodnocení jej vypíše a dál pokračuje v kontrole
+
+Při spuštění skriptu s parametrem `show` skript vypíše názvy a hodnocení (u termínů ještě jejich popis) sledovaných kurzů a termínů
+
+
+Ukázka
+------
+
+Spuštění bez parametru: (ukázka zjištění nového hodnocení)
+
+    $ ./wis.py
+    IAL 0   ->     IAL 15     Půlsemestrální zkouška
+    IAL 30  ->     IAL 45 
+Spuštění s parametrem `show`
+
+    $ ./wis.py show
+    Predmety:
+
+    IAL     0
+    IFJ     10
+    INM     0
+    INP     10
+    ISS     20
+
+    Terminy:
+
+    IAL     0      Půlsemestrální zkouška
+    INP     10     Půlsemestrální zkouška
+Spuštění bez existujícího konfiguračního souboru
+
+    $ ./wis.py show
+    Login: xlogin00
+    Password:
+    Predmety:
+    IAL     0
+    IFJ     10
+    INM     0
+    INP     10
+    ISS     20
+    IDS     0
+    IPK     0
+    IPP     0
+    IZG     0
+    IZU     0
+    ICP     0
+
+`*` hodnoty jsou vymyšlené ;)
+
+`*` výstupy nejsou zatím nic moc
--- wis.py
+++ wis.py
@@ -1,354 +1,246 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-'''
-VUT FIT WIS Checker
-https://github.com/romanblanco/vut-fit-wis-checker
-
-@author:   Roman Blanco
-@contact:  roman.blanco92@gmail.com
-@license:  GNU GPL v3
-'''
-
 import os
 import sys
-import platform
 import signal
 import re
-import urllib.request
 import configparser
+import urllib.request
 import xml.etree.ElementTree as ElementTree
-from time import localtime, strftime, sleep
 from getpass import getpass
+from time import sleep, ctime
+
+
+def main():
+    app = Application()
+    connection = Connection(app.username, app.password)
+    courses = Courses(connection, app.courses)
+    terms = Terms(connection, app.terms)
+    while True:
+        connection.checkConnection()
+        courses.loadCourses()
+        terms.loadTerms()
+        showCond = (len(sys.argv) == 2 and sys.argv[1] == "show")
+        if not showCond:
+            app.checkNewScore(terms, courses)
+            sleep(app.time)
+        else:
+            app.showScore(terms, courses)
+            break
+
 
 class Application:
-
-	def __init__(self):
-		""" Postupne spousti metody """
-
-		self.config()
-
-		while True:
-
-			if self.connection():
-				if self._courseNames: self.courses()
-				if self._termURLs: self.terms()
-				if self._checkUpdate: self.checkNewValuation()
-				if (os.path.isfile("NOVE.log")): self.printNewValuation()
-
-			if self._loop:
-				print("\nAktualizovano: " + strftime("%H:%M:%S", localtime()))
-
-				try: self._loopTime
-				except: self._loopTime = 300
-				for i in range (self._loopTime, 0, -1):
-					sys.stdout.write('\rNasledujici aktualizace: %d ' %i)
-					sys.stdout.flush()
-					sleep(1)
-
-				if (platform.system() == 'Windows'):
-					os.system('cls')
-				else:
-					os.system('clear')
-
-			else:
-				os._exit(0)
-
-
-	def config(self):
-		""" Nacte konfiguraci ze souboru wis.ini, podle konfugurace se 
-		nasledne spousti funkce skriptu """
-
-		config = configparser.ConfigParser()
-		config.read('wis.ini')
-
-		#============== UZIVATELSKE JMENO ===============
-		try: self._username = config['WIS']['user']
-		except:	self._username = input("Login: ")
-		#============== UZIVATELSKE HESLO ===============
-		try: self._password = config['WIS']['pass']
-		except: self._password = getpass(prompt="Password: ", stream=None)
-
-		#============== KONTROLA AKTUALIACI =============
-		try: 
-			if config['WIS']['check'] == 'True':
-				self._checkUpdate = True
-			else:
-				self._checkUpdate = False
-		except:	self._checkUpdate = True
-
-		#============== OPAKOVANI =======================
-		try: 
-			if config['WIS']['loop'] == 'True':
-				self._loop = True
-			else:
-				self._loop = False
-		except: self._loop = True
-
-		#============== OPAKOVANI =======================
-		try: self._loopTime = int(config['WIS']['time'])
-		except:	self._loopTime = 300
-
-		#============== NOTIFIKACE PRI ZMENE ============
-		try: self._notifyCMD = config['WIS']['cmd']
-		except:	self._notifyCMD = ""
-
-		#============== ZOBRAZOVANE KURZY ===============
-		self._courseNames = []
-		try:
-			for course in config['courses']:
-				self._courseNames.append(config['courses'][course])
-		except:
-			self._courseNames = input("Checked courses: ").split(' ')
-		self._courseNames = [c for c in self._courseNames if c != '']
-
-		#============== ZOBRAZOVANE TERMINY =============
-		self._termURLs = []
-		try:
-			for term in config['terms']:
-					self._termURLs.append(config['terms'][term])
-		except:
-			pass
-		self._termURLs = [url for url in self._termURLs if url != '']
-
-	def connection(self):
-		""" Overeni pripojeni k serveru
-
-		return False: nelze se pripojit
-		return True: lze ze pripojit """
-
-		try:
-			urllib.request.urlopen('http://www.fit.vutbr.cz/')
-			return True
-		except:
-			print("! Nelze se pripojit na skolni server")
-			return False
-
-
-	def websiteSource(self, url):
-		""" Ziskani zdrojoveho HTML / XML kodu pro ziskani udaju o
-		hodnoceni
-
-		parametr url: odkaz na stranku jejiz zdrojovy kod se ma vratit
-		return content: Vraci zdrojovy kod stranky (nedekodovany) """
-
-		try:
-			connection = urllib.request.HTTPPasswordMgrWithDefaultRealm()
-			connection.add_password(None, url, self._username, self._password)
-
-			handler = urllib.request.HTTPBasicAuthHandler(connection)
-			opener = urllib.request.build_opener(handler)
-			urllib.request.install_opener(opener)
-
-			content = urllib.request.urlopen(url).read()
-		except:
-			print("! Nelze nacist zdroj stranky")
-			print("! Pravdepodobne byly spatne zadany prihlasovaci udaje")
-			content = None
-
-		return content
-
-
-	def courses(self):
-		""" Ziska hodnoceni kurzu z WISu (XML), vytiskne a ulozi do promenne
-		pro zapsani do souboru """
-
-		url = 'https://wis.fit.vutbr.cz/FIT/st/get-courses.php'
-		source = self.websiteSource(url)
-		if source is None:
-			self.loopOrExit(1)
-		source = source.decode('utf-8')
-
-		try:
-			root = ElementTree.fromstring(source)
-			result = list()
-
-			# Vyhledani prvku 'abbrv' a 'points' v XML kodu
-			for entry in root.iter('course'):
-				result.append([entry.get('abbrv'), int(entry.get('points'))])
-				# result = pole s nactenym hodnocenim
-		except:
-			print("! Problem nacteni XML")
-			self.loopOrExit(1)
-
-		# item[0] = zkratka predmetu podle niz se tridi vypisovane data
-		courses = [ item for item in result if item[0] in self._courseNames]
-
-		print("Predmety:\n")
-
-		courseValuations = ""
-		for course in courses:
-			courseValuations += course[0] + "\t" + str(course[1]) + "\n"
-			# course[0] = zkratka predmetu
-			# course[1] = hodnoceni predmetu
-		print(courseValuations)
-
-		# zapsani zaznamu do souboru kvuli porovnani
-		try:
-			if self._checkUpdate:
-				self.newLogFile(courseValuations)
-		except:
-			# V pripade ze neni nastavena promenna checkUpdate
-			pass
-
-
-	def terms(self):
-		""" Nacte a vytiskne hodnoceni konkretnich terminu """
-
-		print("Terminy:\n")
-		for url in self._termURLs:
-			source = self.websiteSource(url)
-			if source is None:
-				self.loopOrExit(1)
-			source = source.decode('ISO-8859-2')
-
-			courseName = re.search(r'<title>(.{3,4})', source)
-			points = re.search(
-			 r'<td align=right><b>([0-9]+( \(?[0-9]+\)?)?)', source)
-			termName = re.search(r'.*<h[2-3]>(.+)(?= - .+<)', source)
-
-			print(
-			 courseName.group(1) + '\t' +
-			 points.group(1) + '\t' +
-			 termName.group(1)
-			)
-
-
-	def logFile(self):
-		""" Overeni existence souboru s predeslym zaznamem hodnoceni
-
-		return False: soubor neexistuje, nelze porovnavat aktualizace
-		return True: soubor existuje, lze porovnavat aktualizace """
-
-		if (os.path.isfile('wis.log')):
-			return True
-		else:
-			print(
-			 "* Nenalezen soubor s predeslym zaznamem hodnoceni\n"
-			 "* Kontrola aktualizace probehne az pri dalsim spusteni"
-			)
-			return False
-
-
-	def newLogFile(self, courseValuations):
-		""" Zapise do souboru nactene hodnoceni pro pozdejsi
-		porovnavani novych zmen v hodnoceni
-
-		parametr courseValuations: retezec obsahujici hodnoceni ktere
-		ma byt zapsano do noveho souboru """
-
-		if not self.logFile():
-			# neexistuje soubor s predeslym zaznamem hodnoceni
-			with open('wis.log', 'w') as newLogFile:
-				newLogFile.write(courseValuations)
-			newLogFile.close
-			# neni s cim porovnavat zmeny hodnoceni
-			self.loopOrExit(0)
-		else:
-			# zapsani zaznamu ktery bude porovnavan s predeslym
-			with open('wis.new.log', 'w') as newLogFile:
-				newLogFile.write(courseValuations)
-			newLogFile.close
-
-
-	def checkNewValuation(self):
-		""" Porovna soubor s puvodnim a nove nactenym hodnocenim a v
-		pripade rozdilu vytvori novy soubor """
-
-		try:
-			with open('wis.log') as old, open('wis.new.log') as new:
-				oldValues = old.readlines()
-				newValues = new.readlines()
-		except:
-			print("\n! Soubor se zaznamem je prazdny")
-			print("\n! Chyba je zpusobena nevyplnenim kurzu")
-			self.loopOrExit(1)
-
-		changes = ""
-		if len(newValues) == len(oldValues):
-			for line in range(len(newValues)):
-				if newValues[line] != oldValues[line]:
-					changes += newValues[line]
-		else:
-			print(
-			 "\n* Probehla zmena konfigurace\n"
-			 "* Kontrola aktualizace probehne pri pristim spusteni"
-			)
-
-		old.close
-		new.close
-
-		if changes != "":
-			with open('NOVE.log', 'a') as newValFile:
-				newValFile.write(changes)
-			newValFile.close
-			self.notification()
-
-		os.remove('wis.log')
-		os.rename('wis.new.log', 'wis.log')
-
-
-	def printNewValuation(self):
-		""" Pokud existuje soubor s novym hodnocenim, vypise jej """
-
-		with open('NOVE.log', 'r') as newValuation:
-			content = newValuation.read()
-			print("\nNove hodnoceni:\n\n" + content)
-		newValuation.close
-
-
-	def notification(self):
-		try: os.system(self._notifyCMD)
-		except: pass
-
-
-	def loopOrExit(self, exitValue):
-		""" Vhledem ke konfiguraci rozhodne, jestli se skript znovu
-		spusti, nebo ukonci. Pokud je nastavene spousteni ve smycce,
-		zavola metodu __init__(), jinak se ukonci s hodnotou poslanou v
-		parametru
-
-		parametr exitValue: navratova hodnota pri ukonceni """
-
-		try:
-			if self._loop:
-				print("\nAktualizovano: " + strftime("%H:%M:%S", localtime()))
-
-				try:
-					for i in range (self._loopTime, 0, -1):
-						sys.stdout.write('\rNasledujici aktualizace: %d ' %i)
-						sys.stdout.flush()
-						sleep(1)
-				except:
-					for i in range (300, 0, -1):
-						sys.stdout.write('\rNasledujici aktualizace: %d ' %i)
-						sys.stdout.flush()
-						sleep(1)
-
-				if (platform.system() == 'Windows'):
-					os.system('cls')
-				else:
-					os.system('clear')
-				self.__init__()
-			else:
-				os._exit(exitValue)
-		except:
-			os._exit(exitValue)
-
-
-	def signal_handler(signal, frame):
-		""" Pri zachyceni signalu ukonci skript """
-		os._exit(0)
-
-	signal.signal(signal.SIGINT, signal_handler)
-
+    """Zakladni funkce aplikace - nacteni konfiguracnich souboru, kontrola
+    noveho hodnoceni, vypsani hodnoceni
+    """
+
+    def __init__(self):
+        self.username = None
+        self.password = None
+        self.time = 300
+        self.notifyCmd = None
+        self.courses = None
+        self.terms = None
+        self.loadConfig()
+
+    def loadConfig(self):
+        """Ze souboru 'wis.ini' nacte konfiguraci aplikace. Pokud nektere z
+        potrebnych polozek (login, heslo) v souboru nenalezne, bude pozadovat
+        jejich rucni zadani
+        """
+        config = configparser.ConfigParser()
+        try:
+            config.read('wis.ini')
+        except:
+            sys.stderr.write("! Chyba v konfiguracnim souboru\n")
+            os._exit(1)
+        # nacteni nastaveni z konfiguracniho souboru
+        if 'WIS' in config:
+            self.username = config.get('WIS', 'user', fallback=None)
+            self.password = config.get('WIS', 'pass', fallback=None)
+            self.time = int(config.get('WIS', 'time', fallback=300))
+            self.notifyCmd = config.get('WIS', 'cmd', fallback=None)
+        if 'courses' in config:
+            self.courses = config.get('courses', 'course', fallback=None)
+        if self.courses is not None:
+            self.courses = self.courses.split('\n')
+        if 'terms' in config:
+            self.terms = config.get('terms', 'term', fallback=None)
+        if self.terms is not None:
+            self.terms = self.terms.split('\n')
+        # rucni zadani prihl. udaju pokud nebyly v konfiguracnim souboru
+        if self.username is None:
+            self.username = input("Login: ")
+        if self.password is None:
+            self.password = getpass(prompt="Password: ", stream=None)
+
+    def checkNewScore(self, terms, courses):
+        """Porovna stary zaznam hodnoceni s novym a v pripade zmen vypise
+        informaci o novem hodnoceni
+        """
+        # zjisteni zmen v hodnoceni
+        if terms.oldrecord:
+            terms.changes = [(old, new) for old, new in
+                             zip(terms.oldrecord, terms.newrecord)
+                             if new != old]
+        if courses.oldrecord:
+            courses.changes = [(old, new) for old, new in
+                               zip(courses.oldrecord, courses.newrecord)
+                               if new != old]
+        # vypsani zmen + notifikace
+        if terms.changes or courses.changes:
+            if terms.changes:
+                # pole: [predmet] [hodnoceni] [popis]
+                for old, new in terms.changes:
+                    print(old[0] + ' ' + old[1] + '\t->\t' +
+                          new[0] + ' ' + new[1] + '\t' + new[2])
+            if courses.changes:
+                # pole: [predmet] [hodnoceni]
+                for old, new in courses.changes:
+                    print(old[0] + ' ' + old[1] + '\t->\t' +
+                          new[0] + ' ' + new[1])
+            if self.notifyCmd:
+                os.system(self.notifyCmd)
+
+    def showScore(self, terms, courses):
+        """Vypise nactene hodnoceni"""
+        if courses.newrecord:
+            print("Predmety:\n")
+            for course in courses.newrecord:
+                print(course[0] + '\t' + course[1])
+        if courses.newrecord and terms.newrecord:
+            print('')
+        if terms.newrecord:
+            print("Terminy:\n")
+            for term in terms.newrecord:
+                print(term[0] + '\t' + term[1] + '\t' + term[2])
+
+
+class Connection:
+    """Obstarava prace vyzadujici pripojeni k internetu"""
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.status = None
+        self.checkLogin()
+
+    def checkConnection(self):
+        """Kontrola pripojeni. Vraci rizeni az ve chvili, kdy je overene
+        pripojeni
+        """
+        try:
+            testUrl = 'http://www.fit.vutbr.cz/'
+            urllib.request.urlopen(testUrl, timeout=10)
+            self.status = "ok"
+            return
+        except:
+            self.status = "nedostupne"
+            pass
+
+    def websiteSource(self, url, coding):
+        """Ziskani zdrojoveho HTML/XML kodu pro ziskani udaju o
+        hodnoceni
+        """
+        try:
+            connection = urllib.request.HTTPPasswordMgrWithDefaultRealm()
+            connection.add_password(None, url, self.username, self.password)
+            handler = urllib.request.HTTPBasicAuthHandler(connection)
+            opener = urllib.request.build_opener(handler)
+            urllib.request.install_opener(opener)
+            content = urllib.request.urlopen(url).read()
+        except:
+            content = None
+            pass
+        if content is not None:
+            content = content.decode(coding)
+        return content
+
+    def checkLogin(self):
+        """Overeni spravnosti prihlasovacich udaju"""
+        self.checkConnection()
+        testUrl = 'https://wis.fit.vutbr.cz/FIT/st/get-courses.php'
+        source = self.websiteSource(testUrl, 'utf-8')
+        if source is None:
+            sys.stderr.write("! Nepodarilo se prihlasit k WISu\n")
+            os._exit(2)
+
+
+class Courses:
+    """Nacteni informaci o kurzech"""
+
+    def __init__(self, connection, courses):
+        self.oldrecord = None
+        self.newrecord = None
+        self.changes = None
+        self.connection = connection
+        self.courses = courses
+
+    def loadCourses(self):
+        """Uchovani predchoziho hodnoceni kurzu (pokud existuje) a nacteni
+        noveho
+        """
+        url = 'https://wis.fit.vutbr.cz/FIT/st/get-courses.php'
+        source = self.connection.websiteSource(url, 'utf-8')
+        try:
+            root = ElementTree.fromstring(source)
+            result = list()
+            for entry in root.iter('course'):
+                # result = pole s nactenym hodnocenim
+                result.append([entry.get('abbrv'), entry.get('points')])
+        except:
+            sys.stderr.write("! Chyba pri nacitani XML zdroje\n")
+            os._exit(3)
+        # poznamenani stareho hodnoceni a nacteni noveho
+        self.oldrecord = self.newrecord
+        if self.courses is None:
+            self.newrecord = [item for item in result]
+        else:
+            self.newrecord = [item for item in result if item[0]
+                              in self.courses]
+
+
+class Terms:
+    """Nacteni informaci o terminech"""
+
+    def __init__(self, connection, terms):
+        self.oldrecord = None
+        self.newrecord = list()
+        self.changes = None
+        self.connection = connection
+        self.terms = terms
+
+    def loadTerms(self):
+        """Uchovani predchoziho hodnoceni terminu (pokud existuje) a nacteni
+        noveho
+        """
+        if not self.terms:
+            return
+        # poznamenani stareho hodnoceni a nacteni noveho
+        self.oldrecord = self.newrecord
+        self.newrecord = list()
+        for url in self.terms[:]:
+            source = self.connection.websiteSource(url, 'ISO-8859-2')
+            # nacteni informaci z html stranky regexem
+            try:
+                courseName = re.search(r'<title>([A-Z0-9]{3,4})', source)
+                termName = re.search(r'.*<h[2-3]>(.+)(?= - .+<)', source)
+                points = re.search(r'<td align=right><b>'
+                                   '([0-9]+( \(?[0-9]+\)?)?)', source)
+                self.newrecord.append([courseName.group(1), points.group(1),
+                                      termName.group(1), url])
+            except:
+                sys.stderr.write("! Nelze nacist informace o terminu: " +
+                                 url + "\n")
+                self.terms.remove(url)
+                continue
 
 if __name__ == '__main__':
-	# ========================================================
-	# uzitecne pri spousteni skriptu ze slozky pridane v $PATH
-	# -> pro ukladani docasnych souboru do slozky ~/tmp
-	# home = os.path.expanduser("~")
-	# os.chdir(home + "/tmp")
-	# ========================================================
 
-	app = Application()
+    def signal_handler(signal, frame):
+        """Pri zachyceni signalu ukonci skript"""
+        os._exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+    main()
